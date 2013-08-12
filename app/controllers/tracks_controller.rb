44470d9118f7f84a9438c297a6cd39fe57499fd8
Adler Santos <adlersantos712@gmail.com>
2013-08-11 20:01:50 -0700

    implemented CRUD for tracks

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,40 +1,42 @@
 class TracksController < ApplicationController
   def create
-    @track = track.new(params[:track])
+    @track = Track.new(params[:track])
     if @track.save
-      redirect_to tracks_url
+      redirect_to album_url(@track.album)
     else
       errors[:notices] << "track couldn't be saved for some reason"
-      redirect_to new_track_url
+      redirect_to new_album_track_url(@track.album)
     end
   end
 
   def destroy
-    @track = track.find(params[:id])
+    @track = Track.find(params[:id])
     @track.destroy
     redirect_to tracks_url
   end
 
   def edit
-    @track = track.find(params[:id])
-    @bands = Band.all
+    @track = Track.find(params[:id])
+    @album = @track.album
     render :edit
   end
 
   def new
-    @bands = Band.all
+    @album = Album.find(params[:album_id])
+    @album_artist = @album.band
     render :new
   end
 
   def show
-    @track = track.find(params[:id])
-    @tracks = @track.tracks
+    @track = Track.find(params[:id])
     render :show
   end
 
   def update
-    @track = track.find(params[:id])
+    @track = Track.find(params[:id])
     @track.update_attributes(params[:track])
-    redirect_to tracks_url
+
+    @album = Album.find(params[:album_id])
+    redirect_to album_track_url(@album, @track)
   end
 end
