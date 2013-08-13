2f78f3f2325b944072433fac8c4b939837531d07
Adler Santos <adlersantos712@gmail.com>
2013-08-12 21:47:34 -0700

    added view helper for lyrics

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,11 @@
 module TracksHelper
+  def ugly_lyrics(lyrics)
+    lines = lyrics.split("\n")
+
+    lyrics = lines.map do |line|
+      "&#9835; #{line}"
+    end
+
+    lyrics.join("\n").html_safe
+  end
 end
